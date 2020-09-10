0156b70c5687eb3fe1cc48f4ef1185b4ffd2ed31
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-09-10 22:42:48 +0900

    feat: remove buttons

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -7,41 +7,18 @@ import com.mikepenz.itemanimators.ScaleUpAnimator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private val min = 0
-    private var max = 10
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val layoutManager = LinearLayoutManager(applicationContext)
-        val customAdapter =  CustomRecyclerAdapter().apply { data = createSampleData(min, max) }
-        val scaleUpAnimator = ScaleUpAnimator().apply {
-            addDuration = 2000
-            removeDuration = 4000
-        }
+        val customAdapter =  CustomRecyclerAdapter().apply { data = createSampleData() }
 
         recycler_view.also { view ->
             view.adapter = customAdapter
             view.layoutManager = layoutManager
-            view.itemAnimator = scaleUpAnimator
             view.setHasFixedSize(true)
-        }
-
-        plus_button.setOnClickListener {
-            max++
-
-            val new = createSampleData(min, max)
-            customAdapter.data = new
-            customAdapter.notifyItemInserted(max)
-        }
-
-        minus_button.setOnClickListener {
-            max--
-            val new = createSampleData(min, max)
-            customAdapter.data = new
-            customAdapter.notifyItemRemoved(max + 1)
         }
     }
 
-    private fun createSampleData(min: Int, max: Int) = (min..max).map { it.toString() }
+    private fun createSampleData() = (0..100).map { it.toString() }
 }
